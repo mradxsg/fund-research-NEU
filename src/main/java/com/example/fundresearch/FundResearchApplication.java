bdbfed5521b1e6e4c8139556484a90cac9d3bcdf
mradxsg <2464980512@qq.com>
2026-06-22 17:41:11 +0800

    初始化后端项目，完成包结构和配置文件

---
+++ src/main/java/com/example/fundresearch/FundResearchApplication.java
@@ -0,0 +1,11 @@
+package com.example.fundresearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundResearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FundResearchApplication.class, args);
+    }
+}
